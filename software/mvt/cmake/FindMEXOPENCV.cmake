c923897082bf64932fcbd6671d4ed9b6048a4211
changkyu <changkyusong86@gmail.com>
2015-02-02 18:05:55 -0500

    Find MEXOPENCV Done

--- software/mvt/cmake/FindMEXOPENCV.cmake
+++ software/mvt/cmake/FindMEXOPENCV.cmake
@@ -33,21 +33,18 @@ FIND_LIBRARY(MEXOPENCV_MXARRAY_LIBRARY
     PATHS ${MEXOPENCV_ROOT_DIR}/lib
 )
 
-MESSAGE("${MEXOPENCV_DIR} ${MEXOPENCV_INCLUDE_DIRS} ${MEXOPENCV_MXARRAY_LIBRARY}")
 IF(MEXOPENCV_INCLUDE_DIRS)
-MESSAGE("YES")
+    set(MEXOPENCV_FOUND ON)
 ELSE()
-MESSAGE("NO")
+MESSAGE(SEND_ERROR "[Error] Failed to find \"mexopencv\" automatically (Git URL: https://github.com/kyamagu/mexopencv).
+If \"mexopencv\" is correctly installed but this message is shown, please manually edit CMakeLists.txt at this line.
+Or try again with the option -D MEXOPENCV_DIR=<mexopencv_dir>")
+
+    set(MEXOPENCV_FOUND OFF)
+#    set(MEXOPENCV_FOUND ON)
+#    set(MEXOPENCV_DIR "$ENV{HOME}/lib/mexopencv")
+#    set(MEXOPENCV_LIBRARIES "${MEXOPENCV_DIR}/lib")
+#    set(MEXOPENCV_INCLUDE_DIRS "${MEXOPENCV_DIR}/include")
 ENDIF()
 
-#set(MEXOPENCV_FOUND OFF)
-#IF(NOT MEXOPENCV_FOUND)
-#    MESSAGE(SEND_ERROR "[Error] Failed to find \"mexopencv\" automatically (Git URL: https://github.com/kyamagu/mexopencv).
-#If \"mexopencv\" is correctly installed but this message is shown, please manually edit CMakeLists.txt at this line.
-#Or try again with the option -D MEXOPENCV_DIR=<mexopencv_dir>")
-
-#set(MEXOPENCV_FOUND ON)
-#set(MEXOPENCV_DIR "$ENV{HOME}/lib/mexopencv")
-#set(MEXOPENCV_LIBRARIES "${MEXOPENCV_DIR}/lib")
-#set(MEXOPENCV_INCLUDE_DIRS "${MEXOPENCV_DIR}/include")
 
