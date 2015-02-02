c6fbde1a487c124a1e9426b929741fda5902d58c
CHANGKYU SONG <cs1080@tol.cs.rutgers.edu>
2015-02-02 16:26:12 -0500

    FindMexOpencv

--- software/mvt/cmake/FindMEXOPENCV.cmake
+++ software/mvt/cmake/FindMEXOPENCV.cmake
@@ -1,41 +1,53 @@
+# Find mexopencv library path
+#
+# Author: Changkyu Song (changkyusong86@gmail.com, http://sites.google.com/site/changkyusong86)
+
+IF(WIN32)
+    SET(MEXOPENCV_POSSIBLE_ROOT_DIRS
+        ${MEXOPENCV_DIR}
+        "$ENV{SystemDrive}/mexopencv"
+        "$ENV{SystemDrive}/Program Files/mexopencv"
+        "$ENV{SystemDrive}/Program Files (x86)/mexopencv"
+    )
+ELSE(WIN32)
+    SET(MEXOPENCV_POSSIBLE_ROOT_DIRS
+        ${MEXOPENCV_DIR}
+        /usr/local
+        /usr
+        /opt/local
+    )
+ENDIF(WIN32)
+
+FIND_PATH(MEXOPENCV_ROOT_DIR
+    NAMES include/mexopencv.hpp
+    PATHS ${MEXOPENCV_POSSIBLE_ROOT_DIRS}
+)
 
 FIND_PATH(MEXOPENCV_INCLUDE_DIRS
-      NAMES mexopencv.hpp MxArray.hpp
-      PATHS ${MEXOPENCV_DIR}/include)
+    NAMES mexopencv.hpp MxArray.hpp
+    PATHS ${MEXOPENCV_ROOT_DIR}/include
+)
+
 FIND_LIBRARY(MEXOPENCV_MXARRAY_LIBRARY
-      NAMES MxArray
-      PATHS ${MEXOPENCV_DIR}/lib)
+    NAMES MxArray
+    PATHS ${MEXOPENCV_ROOT_DIR}/lib
+)
 
 MESSAGE("${MEXOPENCV_DIR} ${MEXOPENCV_INCLUDE_DIRS} ${MEXOPENCV_MXARRAY_LIBRARY}")
-IF(MEXOPENCV_INCLUDE_DIRS NOT_FOUND)
-
-ENDIF()
+IF(MEXOPENCV_INCLUDE_DIRS)
+MESSAGE("YES")
 ELSE()
-MESSAGE(${MEXOPENCV_DIR})
+MESSAGE("NO")
 ENDIF()
 
 #set(MEXOPENCV_FOUND OFF)
-IF(NOT MEXOPENCV_FOUND)
-    MESSAGE(SEND_ERROR "[Error] Failed to find \"mexopencv\" automatically (Git URL: https://github.com/kyamagu/mexopencv).
-If \"mexopencv\" is correctly installed but this message is shown, please manually edit CMakeLists.txt at this line.
-Or try again with the option -D MEXOPENCV_DIR=<mexopencv_dir>")
+#IF(NOT MEXOPENCV_FOUND)
+#    MESSAGE(SEND_ERROR "[Error] Failed to find \"mexopencv\" automatically (Git URL: https://github.com/kyamagu/mexopencv).
+#If \"mexopencv\" is correctly installed but this message is shown, please manually edit CMakeLists.txt at this line.
+#Or try again with the option -D MEXOPENCV_DIR=<mexopencv_dir>")
 
 #set(MEXOPENCV_FOUND ON)
 #set(MEXOPENCV_DIR "$ENV{HOME}/lib/mexopencv")
 #set(MEXOPENCV_LIBRARIES "${MEXOPENCV_DIR}/lib")
 #set(MEXOPENCV_INCLUDE_DIRS "${MEXOPENCV_DIR}/include")
-
-
-
-
-# display help message
-IF(NOT MEXOPENCV_FOUND)
-    MESSAGE(SEND_ERROR "[Error] Failed to find \"Matlab\" automatically. If \"Matlab\" is correctly installed but this message is shown, please manually edit CMakeLists.txt at this line")
-ENDIF()
-
-#set(MEXOPENCV_FOUND ON)
-#set(MEXOPENCV_DIR "$ENV{HOME}/lib/mexopencv")
-#set(MEXOPENCV_LIBRARIES "${MEXOPENCV_DIR}/lib")
-#set(MEXOPENCV_INCLUDE_DIRS "${MEXOPENCV_DIR}/include")
-
 
